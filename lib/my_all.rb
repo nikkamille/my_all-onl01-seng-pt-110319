593769ec9e7c1a60964d89293bfc25dcfc4fd04f
Kamille Longalong <k.pagibigan@gmail.com>
2019-12-01 09:29:49 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,12 +1,2 @@
 require 'pry'
 
-def my_all?(collection)
-  i = 0
-  block_return_values = []
-  while i < collection.length
-  yield(collection[i])
-    i += 1
-  end
-end
-
-my_all?([1,2,3]) {|i| i < 2}
